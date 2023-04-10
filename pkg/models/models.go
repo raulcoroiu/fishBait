d345585d400a7ad9f6d9d1e9c1ac88aa8d94bd0e
DESKTOP-SCD7G66\Raul <coroiuraul@yahoo.com>
2023-04-10 13:06:36 +0300

    add validate request

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -7,11 +7,11 @@ import (
 
 type User struct{
 	ID					primitive.ObjectID				`json:"_id" bson:"_id"`
-	First_Name 			*string							`json:"first_name"`
-	Last_Name 			*string							`json:"last_name"`
-	Password 			*string							`json:"password"`
-	Email				*string							`json:"email"`
-	Phone				*string							`json:"phone"`
+	First_Name 			*string							`json:"first_name" 		validate:"required,min=2,max=30"`
+	Last_Name 			*string							`json:"last_name" 		validate:"required,min=2,max=3"`
+	Password 			*string							`json:"password" 		validate:"required,min=6"`
+	Email				*string							`json:"email" 			validate:"email,required"`
+	Phone				*string							`json:"phone" 			validate:"required"`
 	Token				*string							`json:"token"`
 	Refresh_Token		*string							`json:"refresh_token"`	
 	Created_At			time.Time						`json:"created_at"`
