f465245d8680fc608e53aa16cba9e1ef5c5be4fe
DESKTOP-SCD7G66\Raul <coroiuraul@yahoo.com>
2023-04-12 14:32:06 +0300

    Creatin controllers func

---
+++ pkg/controllers/adress.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+
+)
+
+func AddAddress() gin.HandlerFunc{
+
+}
+
+func EditAddress() gin.HandlerFunc{
+
+}
+
+func EditWorkAddress() gin.HandlerFunc{
+
+}
+
+func DeleteAddress() gin.HandlerFunc{
+	
+}
